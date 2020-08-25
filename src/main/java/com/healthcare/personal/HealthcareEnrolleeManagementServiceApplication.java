641655e91d49e13e1bd159954fc1691dfe231b11
Yogesh Verma <yogeshverma.ee@gmail.com>
2020-08-25 18:42:32 -0400

    initial structure commit

---
+++ src/main/java/com/healthcare/personal/HealthcareEnrolleeManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.healthcare.personal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthcareEnrolleeManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthcareEnrolleeManagementServiceApplication.class, args);
+	}
+
+}
